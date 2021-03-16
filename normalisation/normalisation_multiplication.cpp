d63759d3ca1ee68db1f581d8f80794a1f9b56b58
nitin <nitindwivedi267@gmail.com>
2021-03-17 02:46:06 +0530

    added more programs

--- normalisation/normalisation_multiplication.cpp
+++ normalisation/normalisation_multiplication.cpp
@@ -4,28 +4,28 @@
 
 using namespace std;
 
-void subtraction(float x1,int e1,float x2,int e2, float*x,int*e)
+void multiplication(float x1,int e1,float x2,int e2, float &x,int &e)
 {
-	float k=abs(e1-e2);
-	if(e1>e2)
-	{
-		x2=x2/pow(10,k);
-		*e=e1;
-	}
-	else
-	{
-		x1=x1/pow(10,k);
-		*e=e2;
-	}
-	*x=x1-x2;
 	
-	while(abs(*x)<0.1 && abs(*x)>0.0)
-	{
-		*x*=10;
-		*e-=1;
-	}
+	x=x1*x2;
+	e=e1+e2;
 	
-	if(*e<-99)
+	if(abs(x)>=1.0)
+	{
+		x/=10;
+		e+=1;
+	}
+	else if(abs(x)<0.1)
+	{
+		x*=10;
+		e-=1;
+	}
+	if(e>99)
+	{	
+		cout<<"\nOverflow!";
+		exit(0);
+	}
+	else if(e<-99)
 	{
 		cout<<"\nUnderflow!";
 		exit(0);
@@ -44,8 +44,8 @@ int main()
 	cout<<"\nEnter 2nd number(mantissa) and its exponential: ";
 	cin>>x2>>e2;
 	cout<<"\nEntered no. is: "<<x2<<"E"<<e2;
-	subtraction(x1,e1,x2,e2,&x,&e);
-	cout<<"\nResultant difference is: "<<setprecision(4)<<x<<"E"<<e;
+	multiplication(x1,e1,x2,e2,x,e);
+	cout<<"\nResultant multiplication is: "<<setprecision(4)<<x<<"E"<<e;
 	
 	return 0;
 	
